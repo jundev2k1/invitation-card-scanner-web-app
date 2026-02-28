28d6a59330044475e7f61bb910ed9719cfe163d2
Jun Dev <jun.dev2001@gmail.com>
2026-02-28 16:51:39 +0700

    chore(user list): change action buton to dropdown button for scalable

--- app/[locale]/(protected)/users/useUserPage.tsx
+++ app/[locale]/(protected)/users/useUserPage.tsx
@@ -3,7 +3,7 @@ import {
   BaseFilter,
   Column,
   defaultBaseFilter,
-  IconButton,
+  DropdownButton,
   SexBadge,
   SmartDateTime,
   TruncatedText,
@@ -11,9 +11,9 @@ import {
   UserStatusBadge
 } from "@/components";
 import { TranslateFn } from "@/i18n/type";
-import { ClockIcon, InfoIcon, MailIcon, PhoneIcon, UserIcon } from "@/icons";
+import { ClockIcon, MailIcon, PhoneIcon, UserIcon } from "@/icons";
 import { useGetUserSearch } from "@/services";
-import { defaultSearchResult, UserSearchItemDto, UserStatus } from "@/types";
+import { defaultSearchResult, PageAction, UserSearchItemDto, UserStatus } from "@/types";
 import { RouteUtil } from "@/utils/route";
 import { useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
@@ -26,63 +26,72 @@ const getBreadcrumbs = (t: TranslateFn, locale: string) => [
   { label: t('user.list.title') },
 ];
 
-const getColumns = (t: TranslateFn, redirectToDetail: (id: string) => void): Column<UserSearchItemDto>[] => [
-  {
-    key: "id",
-    label: t('user.list.table.columns.id'),
-    className: "w-20",
-    render: (_, item) => <TruncatedText className="dark:text-muted-foreground" text={item.id} isUUID showCopy />
-  },
-  {
-    key: "information",
-    label: t('user.list.table.columns.information'),
-    render: (_, item) => (
-      <div className="flex gap-2">
-        <Avatar size="lg">
-          <AvatarImage src={item.avatarUrl} />
-          <AvatarFallback><UserIcon /></AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col gap-1">
-          <p className="flex gap-2">
-            <SexBadge sex={item.sex} />
-            <span className="font-medium dark:text-muted-foreground">{item.nickname} ({item.username})</span>
-          </p>
-          <p className="flex items-center gap-3 dark:text-muted-foreground">
-            <span className="flex items-center gap-1">
-              <MailIcon size={12} />
-              <TruncatedText text={item.email} isTruncate={false} />
-            </span>
-            <span className="flex items-center gap-1">
-              <PhoneIcon size={12} />
-              <TruncatedText text={item.phoneNumber} isTruncate={false} />
-            </span>
-          </p>
-          <p className="flex items-center gap-1 dark:text-muted-foreground">
-            <ClockIcon size={12} />
-            <SmartDateTime date={item.createdAt} label={t('common.datetime.createdAt')} />
-          </p>
+const getColumns = (
+  t: TranslateFn,
+  redirectToDetail: (id: string) => void,
+  redirectToEdit: (id: string) => void
+): Column<UserSearchItemDto>[] => [
+    {
+      key: "id",
+      label: t('user.list.table.columns.id'),
+      className: "w-20",
+      render: (_, item) => <TruncatedText className="dark:text-muted-foreground" text={item.id} isUUID showCopy />
+    },
+    {
+      key: "information",
+      label: t('user.list.table.columns.information'),
+      render: (_, item) => (
+        <div className="flex gap-2">
+          <Avatar size="lg">
+            <AvatarImage src={item.avatarUrl} />
+            <AvatarFallback><UserIcon /></AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col gap-1">
+            <p className="flex gap-2">
+              <SexBadge sex={item.sex} />
+              <span className="font-medium dark:text-muted-foreground">{item.nickname} ({item.username})</span>
+            </p>
+            <p className="flex items-center gap-3 dark:text-muted-foreground">
+              <span className="flex items-center gap-1">
+                <MailIcon size={12} />
+                <TruncatedText text={item.email} isTruncate={false} />
+              </span>
+              <span className="flex items-center gap-1">
+                <PhoneIcon size={12} />
+                <TruncatedText text={item.phoneNumber} isTruncate={false} />
+              </span>
+            </p>
+            <p className="flex items-center gap-1 dark:text-muted-foreground">
+              <ClockIcon size={12} />
+              <SmartDateTime date={item.createdAt} label={t('common.datetime.createdAt')} />
+            </p>
+          </div>
         </div>
-      </div>
-    )
-  },
-  {
-    key: "status",
-    label: t('user.list.table.columns.status'),
-    render: (_, item) => <UserStatusBadge status={item.status} />
-  },
-  {
-    key: "action",
-    label: t('user.list.table.columns.action'),
-    render: (_, item) => (
-      <IconButton
-        variant="ghost"
-        icon={<InfoIcon />}
-        className="cursor-pointer dark:text-muted-foreground"
-        onClick={() => redirectToDetail(item.id)}
-      />
-    )
-  },
-];
+      )
+    },
+    {
+      key: "status",
+      label: t('user.list.table.columns.status'),
+      className: "text-center",
+      align: "center",
+      render: (_, item) => <UserStatusBadge status={item.status} />
+    },
+    {
+      key: "action",
+      label: t('user.list.table.columns.action'),
+      className: "w-[10%] text-right",
+      align: "right",
+      render: (_, item) => {
+        const options = [
+          { label: t('common.actions.view'), action: () => { redirectToDetail(item.id) } },
+          { label: t('common.actions.edit'), action: () => { redirectToEdit(item.id) } },
+        ];
+        return (
+          <DropdownButton options={options} />
+        );
+      }
+    },
+  ];
 
 interface UserFilters extends BaseFilter {
   statuses: UserStatus[]
@@ -115,13 +124,18 @@ export const useUserPage = () => {
     refetch
   } = useGetUserSearch(getSearchParams(filter));
 
-  const redirectToDetail = useCallback((id: string) => router.push(RouteUtil.getUserDetailUrl(locale, id)), [locale]);
+  const redirectToDetail = useCallback((id: string) =>
+    router.push(RouteUtil.getUserDetailUrl(locale, id, PageAction.VIEW)), [locale]);
+
+  const redirectToEdit = useCallback((id: string) =>
+    router.push(RouteUtil.getUserDetailUrl(locale, id, PageAction.EDIT)), [locale]);
+
   const onStatusChange = useCallback((statuses: UserStatus[]) => {
     updateFilter({ ...filter, page: 1, statuses });
   }, [locale]);
 
   const breadcrumbs = useMemo(() => getBreadcrumbs(t, locale), [locale]);
-  const columns = useMemo(() => getColumns(t, redirectToDetail), [locale]);
+  const columns = useMemo(() => getColumns(t, redirectToDetail, redirectToEdit), [locale]);
   const onPageRefresh = useCallback(refetch, []);
   const useStatusOptions = useMemo(() => getUserStatusOptions(t), [t]);
 
